0b47affebd6e799f9db2afaf2e62122e5f519092
Michael Ebert-Hanke <mail@ebert-hanke.de>
2021-10-04 01:43:23 +0200

    basic vignette preview working

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -1,17 +1,41 @@
-use fltk::{enums::*, prelude::*, *};
-use retro_filter_lib::create_vignette;
+use fltk::{
+    app, button,
+    dialog::file_chooser,
+    draw,
+    enums::{Color, ColorDepth},
+    frame,
+    image::{self as fl_image, SharedImage},
+    prelude::*,
+    valuator, window,
+};
+use image::{io::Reader as ImageReader, GenericImageView};
+use retro_filter_lib::{create_vignette, preview};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let img = create_vignette(300);
-    let (w, h) = img.dimensions();
+    let mut image = ImageReader::open("/Users/halo/desktop/image.jpg")
+        .unwrap()
+        .decode()
+        .unwrap();
+    let thumbnail = image.thumbnail(300, 300);
     let app = app::App::default();
-    let mut win = window::Window::default().with_size(w as i32, h as i32);
-    let mut frm = frame::Frame::default().size_of(&win);
+    let mut win = window::Window::default().with_size(900, 600);
+    let mut slider = valuator::Slider::new(800, 10, 30, 500, "slider");
+    let mut frm = frame::Frame::default().with_size(300, 300);
+    slider.set_range(300.0, 1.0);
+    slider.set_step(1.0, 1);
+    win.set_color(Color::White);
     win.end();
     win.show();
     win.make_resizable(true);
-    frm.draw(move |_| {
-        draw::draw_image(&img, 0, 0, w as i32, h as i32, ColorDepth::Rgb8).unwrap();
+    slider.set_callback({
+        move |s| {
+            let preview = preview(thumbnail.clone(), s.value() as i32);
+            let (w, h) = preview.dimensions();
+            let fltk_img =
+                fl_image::RgbImage::new(&preview, w as i32, h as i32, ColorDepth::Rgb8).unwrap();
+            frm.set_image(Some(fltk_img));
+            app::redraw();
+        }
     });
     app.run()?;
     Ok(())
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,64 @@
-use image::{ImageBuffer, Pixel, Rgb, RgbImage};
-use imageproc::drawing::draw_filled_circle_mut;
+use image::{
+    imageops::{blur, overlay},
+    DynamicImage, GenericImageView, ImageBuffer, Rgb, Rgba, RgbaImage,
+};
+use imageproc::drawing::{draw_filled_circle_mut, draw_hollow_circle_mut};
 
-pub fn create_vignette(radius: u32) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
-    let x = radius * 2;
-    let mut vignette = RgbImage::new(x, x);
-    vignette.pixels_mut().into_iter().for_each(|px| px[0] = 255);
+pub fn preview(mut image: DynamicImage, radius: i32) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+    let (width, height) = image.dimensions();
+    let (center_x, center_y) = ((width / 2) as i32, (height / 2) as i32);
+    let mut canvas = RgbaImage::new(width, height);
+    canvas
+        .pixels_mut()
+        .into_iter()
+        .for_each(|px| *px = Rgba([0u8, 0u8, 0u8, 255u8]));
+    let inner = radius / 2;
+    draw_filled_circle_mut(
+        &mut canvas,
+        (center_x, center_y),
+        inner,
+        Rgba([0u8, 0u8, 0u8, 0u8]),
+    );
+    let alpha_step = if inner >= 255 {
+        1
+    } else {
+        (255.0 / inner as f32).ceil() as u32
+    };
+
+    println!("radius:{}", radius);
+    println!("alpha:{}", alpha_step);
+    (1..=inner).rev().into_iter().for_each(|i| {
+        draw_filled_circle_mut(
+            &mut canvas,
+            (center_x, center_y),
+            inner + i,
+            Rgba([0u8, 0u8, 0u8, (alpha_step * i as u32).clamp(0, 255) as u8]),
+        );
+    });
+    overlay(&mut image, &blur(&canvas, 2.0), 0, 0);
+    image.to_rgb8()
+}
+
+pub fn create_vignette(width: u32, height: u32, radius: i32) -> DynamicImage {
+    let outer_box = ((width * width + height * height) as f64).sqrt().ceil() as u32;
+    let mut vignette = RgbaImage::new(outer_box, outer_box);
+    let center = (outer_box / 2) as i32;
+    // println!(
+    //     "Image: {}x{}, Outer Box:{}, Center:{}",
+    //     width, height, outer_box, center
+    // );
+    vignette
+        .pixels_mut()
+        .into_iter()
+        .for_each(|px| *px = Rgba([0u8, 0u8, 0u8, 100u8]));
     draw_filled_circle_mut(
         &mut vignette,
-        (radius as i32, radius as i32),
-        radius as i32,
-        Rgb([0u8, 255u8, 0u8]),
+        (center, center),
+        radius,
+        Rgba([255u8, 255u8, 255u8, 0u8]),
     );
-    vignette
+    //blur(&vignette, 30.0);
+    DynamicImage::ImageRgba8(vignette)
 }
 
 #[cfg(test)]
