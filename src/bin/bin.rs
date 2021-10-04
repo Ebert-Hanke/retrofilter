7088eee1f7e89ca110a95812b99fb01aafddb4c5
Michael Ebert-Hanke <mail@ebert-hanke.de>
2021-10-04 23:39:39 +0200

    save button added, some gui changes

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -1,5 +1,6 @@
 use fltk::{
-    app, button, dialog,
+    app, button,
+    dialog::{self, FileDialogOptions},
     enums::{Color, ColorDepth, FrameType},
     frame,
     frame::Frame,
@@ -16,40 +17,50 @@ use retro_filter_lib::preview;
 #[derive(Debug, Clone, Copy)]
 pub enum Message {
     OpenFile,
+    SaveFile,
     ChangeRadius,
     ChangeOpacity,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // initial data
+    let mut image_data: Option<DynamicImage> = None;
     let mut thumbnail = None;
 
     // setup fltk gui
     // theme
     let app = app::App::default().with_scheme(app::Scheme::Gtk);
-    let theme = ColorTheme::from_colormap(color_themes::DARK_THEME);
-    theme.apply();
+    //let theme = ColorTheme::from_colormap(color_themes::GRAY_THEME);
+    //theme.apply();
     let widget_theme = WidgetTheme::new(ThemeType::AquaClassic);
     widget_theme.apply();
     // define window
     let mut win = window::Window::default()
         .with_size(900, 600)
+        .center_screen()
         .with_label("Retro Filter");
     win.set_color(Color::BackGround);
     win.make_resizable(true);
     // define components
+    let mut input_chooser = dialog::NativeFileChooser::new(dialog::FileDialogType::BrowseFile);
+    input_chooser.set_option(dialog::FileDialogOptions::Preview);
+    input_chooser.set_option(FileDialogOptions::UseFilterExt);
+    input_chooser.set_filter("*.{jpg,jpeg,JPG,png,tif,bmp}");
+    let mut save_chooser = dialog::NativeFileChooser::new(dialog::FileDialogType::BrowseSaveFile);
+    save_chooser.set_option(dialog::FileDialogOptions::SaveAsConfirm);
     let mut btn_open_file = button::Button::new(10, 430, 100, 20, "Open File");
+    let mut btn_save_file = button::Button::new(130, 430, 100, 20, "Save File");
     let mut frm = frame::Frame::new(10, 10, 400, 400, None);
     frm.set_frame(FrameType::BorderBox);
     frm.set_color(Color::Dark1);
     let mut v_slider_radius = valuator::Slider::new(430, 10, 20, 400, "Radius");
-    v_slider_radius.set_range(200.0, 1.0);
+    v_slider_radius.set_range(300.0, 50.0);
     v_slider_radius.set_step(1.0, 1);
-    v_slider_radius.set_value(100.0);
+    v_slider_radius.set_value(150.0);
     let mut v_slider_opacity = valuator::Slider::new(480, 10, 20, 400, "Opacity");
-    v_slider_opacity.set_range(255.0, 1.0);
+    v_slider_opacity.set_range(255.0, 50.0);
     v_slider_opacity.set_step(1.0, 1);
-    v_slider_opacity.set_value(100.0);
+    v_slider_opacity.set_value(127.0);
     // end setup and display window
     win.end();
     win.show();
@@ -57,6 +68,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // event handling
     let (s, r) = app::channel::<Message>();
     btn_open_file.emit(s, Message::OpenFile);
+    btn_save_file.emit(s, Message::SaveFile);
     v_slider_radius.emit(s, Message::ChangeRadius);
     v_slider_opacity.emit(s, Message::ChangeOpacity);
 
@@ -66,21 +78,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             match msg {
                 // functionality
                 Message::OpenFile => {
-                    let file = dialog::file_chooser(
-                        "Select Input File",
-                        "*.{jpg,jpeg,png,tif,bmp}",
-                        ".",
-                        true,
-                    )
-                    .unwrap();
-                    let image = ImageReader::open(file).unwrap().decode().unwrap();
-                    thumbnail = Some(image.thumbnail(400, 400));
-
-                    // draw initial view
-                    if let Some(thumbnail) = &thumbnail {
-                        draw_image(thumbnail, &mut frm, &v_slider_radius, &v_slider_opacity);
-                        app::redraw();
+                    input_chooser.show();
+                    let input_path = input_chooser.filename();
+                    if input_path.is_file() {
+                        image_data = Some(ImageReader::open(input_path).unwrap().decode().unwrap());
+                        if let Some(image) = &image_data {
+                            thumbnail = Some(image.thumbnail(400, 400));
+                        }
+                        // draw initial view
+                        if let Some(thumbnail) = &thumbnail {
+                            draw_image(thumbnail, &mut frm, &v_slider_radius, &v_slider_opacity);
+                            app::redraw();
+                        }
                     }
+                }
+                Message::SaveFile => {
+                    save_chooser.show();
+                    let save_path = save_chooser.filename();
                 }
                 Message::ChangeRadius => {
                     if let Some(thumbnail) = &thumbnail {
@@ -109,7 +123,7 @@ fn draw_image(
 ) {
     let preview = preview(
         thumbnail,
-        v_slider_radius.value() as i32,
+        v_slider_radius.value() as u32,
         v_slider_opacity.value() as u8,
     );
     let (w, h) = preview.dimensions();
