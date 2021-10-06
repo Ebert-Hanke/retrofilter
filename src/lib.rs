82c1a4db8f5914387968c00a85aacad92ff83e86
Michael Ebert-Hanke <mail@ebert-hanke.de>
2021-10-06 23:47:05 +0200

    palette crate successfully implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-use image::{DynamicImage, GenericImageView, ImageBuffer, Rgb};
+use image::{DynamicImage, GenericImage, GenericImageView, ImageBuffer, Rgb};
 mod vignette;
+use palette::{Hsl, Hue, IntoColor, Pixel, Saturate, Shade, Srgb};
 use vignette::create_vignette;
 
 pub fn process_image(
@@ -11,40 +12,23 @@ pub fn process_image(
 ) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
     let (width, height) = image.dimensions();
     let vignette = create_vignette(width, height, radius);
-    let mut base_image = image.clone();
-    //overlay(&mut base_image, &vignette, 0, 0);
-    //base_image.to_rgb8()
-    darken(base_image, vignette).to_rgb8()
+    let mut base_image = image.clone().to_rgb8();
+    darken(&mut base_image, vignette);
+    base_image
 }
 
-pub fn darken(base_image: DynamicImage, top_image: DynamicImage) -> DynamicImage {
-    let mut base = base_image.to_rgb8();
-    //  let mut base_raw = base_image.into_bytes();
-    //  let color_buffer: &mut [Srgb<u8>] = Pixel::from_raw_slice_mut(&mut base_raw);
-    //  let base: Hsv = color_buffer.into_color();
-    // let (w, h) = base.dimensions();
-    let top = top_image.into_luma8();
-    let (w, h) = base.dimensions();
-
-    todo!()
-    // let strength = 1.0;
-    // base.pixels_mut()
-    //     .into_iter()
-    //     .zip(top.pixels().into_iter())
-    //     .for_each(|(b, t)| {
-    //         let base: Hsv = Srgb::from(b);
-    //         let top: Hsv = Srgb::from(t);
-    //         let blend = base.blend(b, multiply);
-    //         let mut blend = Rgb([0, 0, 0]);
-    //         (0..3).into_iter().for_each(|i| {
-    //             if b[i] > t[0] {
-    //                 blend[i] = ((b[i] as u32 + t[0] as u32) / 2).clamp(0, 255) as u8;
-    //             } else {
-    //                 blend[i] = b[i];
-    //             }
-    //         });
-    //         *b = blend
-    //     });
-    // let result = base;
-    // DynamicImage::ImageRgb8(result)
+pub fn darken(base_image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, top_image: DynamicImage) {
+    base_image.pixels_mut().into_iter().for_each(|px| {
+        // decode from raw to palette Srgb
+        let pp = Srgb::from_raw(&px.0);
+        // convert to Hsl
+        let hsl: Hsl = pp.into_format().into_color();
+        // modify
+        let shift = hsl.shift_hue(80.0);
+        // convert back to Srgb
+        let rgb: Srgb = shift.into_color();
+        // encode back to raw
+        let raw: [u8; 3] = Srgb::into_raw(rgb.into_format());
+        px.0 = raw;
+    });
 }
